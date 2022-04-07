06e0ec824d59bf01b226e1956030c10e9717b69e
gumennikov2002 <gumennikov2002@yandex.ru>
2022-04-07 20:05:29 +0400

    Redesign

--- core/Controller.js
+++ core/Controller.js
@@ -7,7 +7,6 @@ class Controller {
 
 	init() {
 		this.drawGames()
-		console.log(window.location)
 	}
 
 	drawGames() {
@@ -35,7 +34,7 @@ class Controller {
 		Object.keys(games).forEach((item) => {
 			let game = games[item]
 
-			app.querySelector('.row').innerHTML += `
+			this.app.querySelector('.row').innerHTML += `
 				<div class="game-box" data-game=${game.name}>
 					<div class="game-img">
 						<img src="${game.img}" ondragstart="return false">
@@ -49,17 +48,32 @@ class Controller {
 
 		gameBoxes.forEach((element) => {
 			element.addEventListener('click', () => {
-				this.openGame(element.getAttribute('data-game'))
+				this.openGame(element.getAttribute('data-game'), element.querySelector('span').innerHTML)
 			})
 		})
 	}
 
-	openGame(name) {
-		// this.app.innerHTML = ''
-		axios.get(this.baseUrl + '/games/snake.html')
-		.then((result) => {
-			console.log(result)
+	openGame(name, title) {
+		this.app.innerHTML = `
+			<div class="d-flex justify-content-between mb-2">
+				<i class="pt-2 fa fa-backward text-dark back-from-game"></i>
+				<h2 class="pt-2 text-center">${title}</h2>
+				<div></div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 game-frame" id="gameFrame">
+				</div>
+			</div>
+		`
+		console.log(this.app.querySelector('.back-from-game'))
+		this.app.querySelector('.back-from-game').addEventListener('click', () => {
+			this.app.innerHTML = `<div class="row d-flex justify-content-around pt-5"></div>`
+			this.drawGames()
 		})
+		// axios.get(this.baseUrl + '/games/snake.html')
+		// .then((result) => {
+		// 	console.log(result)
+		// })
 	}
 }
 
